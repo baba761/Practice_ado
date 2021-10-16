05a5211f38ece3f44620729f888de3b313aa1b8c
RohitGoswami <rohitgoswamigi@gmail.com>
2021-10-16 13:15:14 +0530

    first Commit

---
+++ Practice_ado/ConnectionString.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System;
+
+
+namespace Practice_ado
+{
+    public class ConnectionString
+    {
+        public static void Connection()
+        {
+            SqlConnection conn = null;
+            string cs = ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+            try
+            {
+                using (conn = new SqlConnection(cs))
+                {
+                    string querry = "select * from Employe.Employes";
+                    SqlCommand cmd = new SqlCommand(querry,conn);
+                    conn.Open();
+                    SqlDataReader dr= cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Console.WriteLine("id = " + dr["id"] + " Name = " + dr["Name"]);
+                    }
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+            }
+        } 
+    }
+}
